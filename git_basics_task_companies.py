29835bc846bcb5d54a552ea8263f47b87399aa20
Serhii.h <serhii.h@Serhii.local>
2023-01-19 10:23:26 +0200

    second task done

---
+++ git_basics_task_companies.py
@@ -0,0 +1,8 @@
+# You have 2 companies with people.
+# One of the companies, let it be Eleks, was taken over by Toshiba.
+# Show it in code. Keep in mind that people with the same name can be in both companies
+
+Deflex = ["Frederic", "Mariel", "Trump", "Jessica", "Mariel", "Jack"]
+Eleks = ["Jack", "Miranda", "Daniel", "Josee", "Jack", "Trump"]
+Toshiba = Eleks.copy()
+print(Toshiba)
