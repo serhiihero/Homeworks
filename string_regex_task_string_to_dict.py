ca2c1e34ba870a01dbeeb5db0ea187cdc903f1e6
serhiihero <serhiiherasymovych+93@gmail.com>
2023-01-25 21:42:34 +0200

    third task of the HW4 done

---
+++ string_regex_task_string_to_dict.py
@@ -0,0 +1,10 @@
+# there is a string " name=Amanda=sssss&age=32&&salary=1500&currency=euro ".
+# Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: euro}
+import re
+
+persons = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
+persons_ref = re.sub(r's{5}', '', persons)
+persons_split = re.split(r'\W', persons_ref)
+while "" in persons_split:
+    persons_split.remove('')
+persons_dict = dict(zip(persons_split[::2], persons_split[1::2]))
