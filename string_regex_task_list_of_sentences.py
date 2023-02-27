f5684240c82eca2973bde33aaadc31aa16bca28b
serhiihero <serhiiherasymovych+93@gmail.com>
2023-02-27 14:25:49 +0200

    fixed 2nd found issue

--- string_regex_task_list_of_sentences.py
+++ string_regex_task_list_of_sentences.py
@@ -1,10 +1,20 @@
 # you have a text break the text(attached file) into sentences.
 # As a result, you should get a list of sentences.
+# The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was launched into low Earth orbit in 1990 and remains in operation.... It was not the first space telescope, but it is one of the largest and most versatile, renowned both as a vital research tool and as a public relations boon for astronomy. The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA's Great Observatories..... The Space Telescope Science Institute (STScI) selects Hubble's targets and processes the resulting data, while the Goddard Space Flight Center (GSFC) controls the spacecraft.A commission headed by Lew Allen, director of the Jet Propulsion Laboratory, was established to determine how the error could have arisen. The Allen Commission found that a reflective null corrector, a testing device used to achieve a properly shaped non-spherical mirror, had been incorrectly assembled—one lens was out of position by 1.3 mm (0.051 in).
 import re
 
-# The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was launched into low Earth orbit in 1990 and remains in operation.... It was not the first space telescope, but it is one of the largest and most versatile, renowned both as a vital research tool and as a public relations boon for astronomy. The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA's Great Observatories..... The Space Telescope Science Institute (STScI) selects Hubble's targets and processes the resulting data, while the Goddard Space Flight Center (GSFC) controls the spacecraft.A commission headed by Lew Allen, director of the Jet Propulsion Laboratory, was established to determine how the error could have arisen. The Allen Commission found that a reflective null corrector, a testing device used to achieve a properly shaped non-spherical mirror, had been incorrectly assembled—one lens was out of position by 1.3 mm (0.051 in).
+given_data = """The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was 
+launched into low Earth orbit in 1990 and remains in operation.... 
+It was not the first space telescope, but it is one of the largest and most versatile, 
+renowned both as a vital research tool and as a public relations boon for astronomy. 
+The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA\'s Great Observatories..... 
+The Space Telescope Science Institute (STScI) selects Hubble\'s targets and processes the resulting data, 
+while the Goddard Space Flight Center (GSFC) controls the spacecraft.A commission headed by Lew Allen,  
+director of the Jet Propulsion Laboratory, was established to determine how the error could have arisen. 
+The Allen Commission found that a reflective null corrector, a testing device used to achieve a properly 
+shaped non-spherical mirror, had been incorrectly assembled—one lens was out of position 
+by 1.3 mm (0.051 in)."""
 
-first_string = 'The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was launched into low Earth orbit in 1990 and remains in operation.... It was not the first space telescope, but it is one of the largest and most versatile, renowned both as a vital research tool and as a public relations boon for astronomy. The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA\'s Great Observatories..... The Space Telescope Science Institute (STScI) selects Hubble\'s targets and processes the resulting data, while the Goddard Space Flight Center (GSFC) controls the spacecraft.A commission headed by Lew Allen, director of the Jet Propulsion Laboratory, was established to determine how the error could have arisen. The Allen Commission found that a reflective null corrector, a testing device used to achieve a properly shaped non-spherical mirror, had been incorrectly assembled—one lens was out of position by 1.3 mm (0.051 in).'
-single_dots_remover = (re.sub(r'\.(?=S)|\.(?=T)', ' ', first_string))
-pack_dots_remover = (re.sub(r'\.\.+', '.', single_dots_remover))
-print(pack_dots_remover)
+replace_data = given_data.replace(r'\n', '')
+split_data = re.split(r'\.\s|\.(?=A commission)', replace_data)
+print(split_data)
