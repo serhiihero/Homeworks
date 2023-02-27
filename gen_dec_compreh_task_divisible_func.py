cf938e67602219b401b442864228001dd65cba79
serhiihero <serhiiherasymovych+93@gmail.com>
2023-02-27 13:43:12 +0200

    fixed found issues

--- gen_dec_compreh_task_divisible_func.py
+++ gen_dec_compreh_task_divisible_func.py
@@ -1,11 +1,6 @@
 # Find all of the numbers from 1-1000 that are divisible by 7
 
-def divisible_num_filter_func():
-    custom_generator = (x for x in range(1, 1000))
-    for data in custom_generator:
-        if data % 7 == 0:
-            yield data
-
+custom_compr = [x for x in range(1, 1001) if x % 7 == 0]
 
 if __name__ == '__main__':
-    divisible_num_filter_func()
+    print(custom_compr)
--- gen_dec_compreh_task_finder_func.py
+++ gen_dec_compreh_task_finder_func.py
@@ -1,11 +1,5 @@
 # Find all of the numbers from 1-1000 that have a 3 in them
-
-def divisible_num_filter_func():
-    custom_generator = (x for x in range(1, 1000))
-    for data in custom_generator:
-        if '3' in str(data):
-            yield data
-
+custom_compr = [x for x in range(1, 1001) if '3' in str(x)]
 
 if __name__ == '__main__':
-    divisible_num_filter_func()
+    print(custom_compr)
