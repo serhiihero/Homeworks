1dedde3d47106ebd5aa11d1d51c89c77af39ed86
serhiihero <serhiiherasymovych+93@gmail.com>
2023-02-27 13:58:21 +0200

    fixed 1st found issue

--- string_regex_task_list_of_names.py
+++ string_regex_task_list_of_names.py
@@ -2,8 +2,11 @@
 # ["FirstItem", "FriendsList", "MyTuple"]
 # convert it to a list of variable names for python in snake case format
 # ["first_item", "friends_list", "my_tuple"]
+import re
 
 list_of_names = ["FirstItem", "FriendsList", "MyTuple"]
-string_of_names = ", ".join(list_of_names).lower()
-string_to_list = string_of_names.split(", ")
-print(string_to_list)
+pattern = re.compile(r'([A-Z][a-z]+)([A-Z][a-z]+)')
+divide_set_of_names = pattern.sub(r'\1_\2', ','.join(list_of_names)).lower()
+
+result = divide_set_of_names.split(',')
+print(divide_set_of_names)
