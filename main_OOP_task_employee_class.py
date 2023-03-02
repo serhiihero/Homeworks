e3fa5877b9ee7f94ebd91918ba52d4854a660a41
serhiihero <serhiiherasymovych+93@gmail.com>
2023-03-02 09:44:32 +0200

    added small changes to the vars

--- main_OOP_task_employee_class.py
+++ main_OOP_task_employee_class.py
@@ -2,66 +2,66 @@
 
 class Employee:
 
-    def __init__(self, name, sex, age, education, employment):
-        self.__name = name
-        self.__sex = sex
-        self.__age = age
-        self.__education = education
+    def __init__(self, employee_name, employee_sex, employee_age, employee_education, employment):
+        self.__employee_name = employee_name
+        self.__employee_sex = employee_sex
+        self.__employee_age = employee_age
+        self.__employee_education = employee_education
         self.__employment = employment
 
     @property
     def name(self):
-        if isinstance(self.__name, str) and len(self.__name) <= 20:
-            return self.__name
+        if isinstance(self.__employee_name, str) and len(self.__employee_name) <= 20:
+            return self.__employee_name
         else:
             raise TypeError('No longer than 20 letters expected for the employee name.')
 
     @name.setter
     def name(self, new_employee_name):
         if isinstance(new_employee_name, str) and len(new_employee_name) <= 20:
-            self.__name = new_employee_name
+            self.__employee_name = new_employee_name
         else:
             raise TypeError('No longer than 20 letters expected for the employee name.')
 
     @property
     def sex(self):
-        if isinstance(self.__sex, str) and self.__sex.lower() in ["male", "female"]:
-            return self.__sex
+        if isinstance(self.__employee_sex, str) and self.__employee_sex.lower() in ["male", "female"]:
+            return self.__employee_sex
         else:
             raise SyntaxError('Sex must be "male" or "female".')
 
     @sex.setter
     def sex(self, employee_sex_change):
         if isinstance(employee_sex_change, str) and employee_sex_change.lower() in ["male", "female"]:
-            self.__sex = employee_sex_change
+            self.__employee_sex = employee_sex_change
         else:
             raise SyntaxError('Sex must be "male" or "female".')
 
     @property
     def age(self):
-        if isinstance(self.__age, int) and 18 <= self.__age <= 60:
-            return self.__age
+        if isinstance(self.__employee_age, int) and 18 <= self.__employee_age <= 60:
+            return self.__employee_age
         else:
             raise TypeError('Only over 18 and no over 60 are allowed.')
 
     @age.setter
     def age(self, employee_age_change):
         if isinstance(employee_age_change, int) and 18 <= employee_age_change <= 60:
-            self.__age = employee_age_change
+            self.__employee_age = employee_age_change
         else:
             raise TypeError('Only over 18 and no over 60 are allowed.')
 
     @property
     def education(self):
-        if isinstance(self.__education, str) and len(self.__education) <= 30:
-            return self.__education
+        if isinstance(self.__employee_education, str) and len(self.__employee_education) <= 30:
+            return self.__employee_education
         else:
             raise TypeError('Education must be a string with no more than 30 characters.')
 
     @education.setter
     def education(self, employee_education_change):
         if isinstance(employee_education_change, str) and len(employee_education_change) <= 30:
-            self.__education = employee_education_change
+            self.__employee_education = employee_education_change
         else:
             raise TypeError('Education must be a string with no more than 30 characters.')
 
